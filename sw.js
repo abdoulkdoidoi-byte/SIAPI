c0a97b0fb97000b57f80b5efe9887b7970a06260
abdoulkdoidoi-byte <abdoulk.doidoi@gmail.com>
2026-02-19 15:45:53 +0000

    Update sw.js
    Signed-off-by: abdoulkdoidoi-byte <abdoulk.doidoi@gmail.com>

--- sw.js
+++ sw.js
@@ -1,11 +1,7 @@
-const CACHE_NAME = 'siapi-v2';
-const assets = ['./', './index.html', './manifest.json'];
-
+const CACHE_NAME = 'siapi-v3';
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['./', './index.html', './script.js', './manifest.json'])));
 });
-
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
-
