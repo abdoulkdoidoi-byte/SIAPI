09ae2f7bf4a51460519cf026d523d89253fb72c0
abdoulkdoidoi-byte <abdoulk.doidoi@gmail.com>
2026-02-19 01:03:26 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,33 @@
-const CACHE_NAME = 'siapi-v2';
+const CACHE_NAME = 'siapi-platinum-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  'https://cdn-icons-png.flaticon.com'
+  'index.html',
+  'manifest.json',
+  'confidentialite.html'
 ];
 
+// Installation du Service Worker
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
+// Activation et nettoyage des anciens caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Stratégie : Réseau d'abord, sinon Cache (pour que le VPN et le Solde soient à jour)
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
   );
 });
-
